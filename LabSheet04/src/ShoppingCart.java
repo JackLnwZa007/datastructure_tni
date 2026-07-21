eb2421dca0bbde85b9b01303a1f06a36bc6101a5
JackLnwZa007 <sirawit.pungkumnun@gmail.com>
2026-07-21 16:42:37 +0700

    2513370011 Submit LabSheet04

---
+++ LabSheet04/src/ShoppingCart.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        LinkedList<String> cart = new LinkedList<String>();
+
+        cart.add("Wireless Mouse");
+
+        cart.add("Mechanical Keyboard");
+
+        cart.add("USB-C Hub");
+
+        System.out.println(cart);
+
+        cart.add(1, "Mouse Pad");
+
+        System.out.println(cart);
+
+        cart.removeLast();
+
+        System.out.println(cart);
+
+        cart.remove("Mouse Pad");
+
+        System.out.println(cart);
+
+        cart.add("Monitor");
+
+        System.out.println(cart);
+
+        int index = cart.indexOf("Monitor");
+        cart.set(index, "Phone Holder");
+
+        System.out.println(cart);
+
+        System.out.println(cart.getLast());
+    }
+}
---
+++ LabSheet04/src/ToDoList.java
@@ -0,0 +1,26 @@
+public class ToDoList {
+    public static void main(String[] arg) {
+        DoublyLinkedList todoList = new DoublyLinkedList();
+
+        todoList.insert("Finish Homework");
+        todoList.insert("Laundry");
+        todoList.insert("Group Meeting");
+        System.out.println("To-DO List =" + todoList.traversal());
+
+        todoList.insert(0 ,"Submit Report");
+        System.out.println("To-Do List =" + todoList.traversal());
+        todoList.insert(2 ,"Buy Food");
+        System.out.println("To-Do List =" + todoList.traversal());
+        todoList.insert("Go to Gym");
+        System.out.println("To-Do List =" + todoList.traversal());
+
+        todoList.remove();
+        System.out.println("To-Do List =" + todoList.traversal());
+        todoList.remove(3);
+        System.out.println("To-Do List =" + todoList.traversal());
+
+        todoList.remove(0);
+        System.out.println("To-Do List =" + todoList.traversal());
+
+    }
+}
